0176ddee992231ded500da4816328fc2d80ba7fe
Relock <hupuk3223@gmail.com>
2020-01-29 19:10:01 +1000

    done

--- 1_exmple.py
+++ 1_exmple.py
@@ -1,7 +1,16 @@
 import turtle
 
+turtle.shape('turtle')
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(50)
 def example():
     pass
-
 if __name__ == "__main__":
     example()
